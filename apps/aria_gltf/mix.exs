7ca7b61bf015c1b6f1192853d5942f5c728ee14e
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2025-11-02 22:02:05 -0800

    Add aria_gltf mix.exs and complete umbrella structure

---
+++ apps/aria_gltf/mix.exs
@@ -0,0 +1,38 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2025-present K. S. Ernest (iFire) Lee
+
+defmodule AriaGltf.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :aria_gltf,
+      version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+      elixir: "~> 1.18",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {AriaGltf.Application, []}
+    ]
+  end
+
+  defp deps do
+    [
+      {:jason, "~> 1.4"},
+      {:nx, "~> 0.10"},
+      {:torchx, "~> 0.10"},
+      {:aria_math, git: "https://github.com/V-Sekai-fire/aria-math.git"},
+      {:aria_joint, in_umbrella: true}
+    ]
+  end
+end
+
